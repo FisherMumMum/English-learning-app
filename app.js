43606661a87b97b7d420495ce40d68bb813d4318
Duy Trương Ngọc <ngoc250604@gmail.com>
2023-12-21 22:23:19 +0700

    my changes

--- app.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express')
-const app = express()
-
-app.get('/',(reg,res)=>{
-    res.send('Hello cac ban!')
-})
-
-app.listen(3000, () => console.log('Its working on port 3000'))
